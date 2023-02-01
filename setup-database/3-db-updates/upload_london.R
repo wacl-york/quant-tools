74fb40bfa2dc91f273befca00e14ac4fedf15d25
Stuart Lacy <stuart.lacy@york.ac.uk>
2023-02-01 14:20:46 +0000

    Added London unratified Ref 2022

--- setup-database/3-db-updates/upload_london.R
+++ setup-database/3-db-updates/upload_london.R
@@ -50,3 +50,67 @@ new_to_upload <- new |> filter(time > max(old$time)) |>
                            sensornumber=1, calibrationname="Ratified",
                            instrument=sprintf("Ref_London_%s", measurand))
 dbAppendTable(con, "measurement", new_to_upload)
+
+# Add 2022 data
+df_2022 <- read_csv("~/GoogleDrive/WACL/BOCS/Calibration/Analyses/data/Reference/London/Max_email_20230124/HOP quant 2022.csv")
+colnames(df_2022) <- gsub("@HP1", "", colnames(df_2022))
+df_2022 <- df_2022 |> 
+            rename(time = `Date&Time`,
+                   PM2.5=PM25,
+                   NOy=NOY) |>
+            mutate(time=as_datetime(time, format="%d-%m-%y %H:%M", tz="UTC"))
+# Current max time in DB is 2022-03-05 23:00 and we have minutely
+tbl(con, "ref") |>
+    filter(location == 'London',
+           measurand == 'O3') |>
+    arrange(desc(time)) |> head(20)
+
+# We have data from 2022-03-01 but 15 minutely
+# Let's check the data overlaps
+old <- tbl(con, "ref") |>
+    filter(location == 'London', time >= as_datetime("2022-03-01")) |>
+    collect() |>
+    mutate(time = floor_date(time, "15 minutes")) |>
+    group_by(time, measurand) |>
+    summarise(measurement = mean(measurement, na.rm=T)) |>
+    ungroup() 
+new <- df_2022 |>
+        filter(time >= min(old$time), time <= max(old$time)) |>
+        pivot_longer(-time, names_to="measurand", values_to="measurement")
+
+# Most differences are very small - less than 1ppb
+# NO has a day or so where the new data has been increased by a few ppb
+old |>
+    rename(old=measurement) |>
+    inner_join(new |> rename(new=measurement), by=c("time", "measurand")) |>
+    mutate(diff = old - new) |>
+    ggplot(aes(x=time, y=diff, colour=measurand)) +
+        geom_line()
+# And in the context of the full time-range, the differences in NO are proportionally very low
+old |>
+    mutate(dataset="old") |>
+    rbind(new |> mutate(dataset="new")) |>
+    ggplot(aes(x=time, y=measurement, colour=dataset)) +
+        geom_line() +
+        facet_wrap(~measurand, scales="free")
+
+# So I can add these data to the dataset for now, maybe from the maximum time onwards
+# But it would be nice to get minutely when available!
+# Prepare for DB upload
+to_upload <- df_2022 |>
+    filter(time > max(old$time)) |>
+    pivot_longer(-time, names_to="measurand", values_to="measurement") |>
+    mutate(sensornumber=1, calibrationname="Unratified2022",
+           instrument=sprintf("Ref_London_%s", measurand),
+           instrument = gsub("PM.+", "PM", instrument)) |>
+    select(instrument, measurand, sensornumber, calibrationname, time, measurement)
+
+# Firstly add Unratified calibration versions
+dbAppendTable(con, "sensorcalibration", 
+              to_upload |> 
+                  distinct(instrument, measurand, sensornumber, calibrationname) |>
+                  mutate(dateapplied = as_datetime("2023-01-24"))
+                  )
+# Now upload data
+dbAppendTable(con, "measurement", to_upload)
+
