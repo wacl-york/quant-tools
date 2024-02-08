60f1ba6267c2b28b701c40304a48528a9b29752c
Stuart Lacy <stuart.lacy@york.ac.uk>
2024-02-08 11:31:17 +0000

    Update instrument_list.csv
    Made the requested changes:
      - Change study_id column name to system_id
      - Added the RLS instruments
      - Removed the Clarity instruments from the separate study
      - Renamed the studies

--- export-for-CEDA/lcs_instrument_table.R
+++ export-for-CEDA/lcs_instrument_table.R
@@ -3,19 +3,25 @@ library(tidyverse)
 library(odbc)
 con <- dbConnect(odbc(), "QUANT")
 
+study_table <- tribble(
+    ~study_old, ~study_new,
+    "QUANT", "Main QUANT",
+    "Wider Participation", "Wider Participation Study"
+)
+
 df <- tbl(con, "lcsinstrument") |>
     inner_join(tbl(con, "lcscompany")) |>
     inner_join(tbl(con, "sensor")) |>
     mutate(measurand = str_replace(measurand, "RelHumidity", "Relative Humidity")) |>
-    filter(company != 'RLS') |>
     distinct(instrument, companyhumanreadable, modelname, url, internalid, study, measurand) |>
     group_by(instrument, companyhumanreadable, modelname, url, internalid, study) |>
     summarise(sensors = str_flatten(measurand, collapse=", ")) |>
     ungroup() |>
-    select(study_id = instrument, study, manufacturer=companyhumanreadable, model=modelname, url, serial=internalid, sensors) |>
+    select(system_id = instrument, study, manufacturer=companyhumanreadable, model=modelname, url, serial=internalid, sensors) |>
     collect() |>
+    inner_join(study_table, by=c("study"="study_old")) |>
     mutate(description = sprintf("Low-cost instrument (make: %s, model %s) measuring air quality as part of the QUANT %s study. Has sensors that measure: %s.", 
                                  manufacturer, model, ifelse(study == 'QUANT', 'main', study), sensors)) |>
-    select(-sensors) |>
-    arrange(manufacturer, study_id)
+    select(system_id, study=study_new, manufacturer, model, url, serial, description) |>
+    arrange(manufacturer, system_id)
 write_csv(df, "export-for-CEDA/lcs_instruments.csv")
