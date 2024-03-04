d011033c55a6e9170d1187626925f441dc56e740
Stuart Lacy <stuart.lacy@york.ac.uk>
2024-03-04 15:53:34 +0000

    feat: changes to CEDA repo for Feb 2024 (#11)
    * Added code to export CSV
    
    NB: This code was written and run in Summer 2023, it just wasn't
    committed then.
    
    * Update instrument_list.csv
    
    Made the requested changes:
      - Change study_id column name to system_id
      - Added the RLS instruments
      - Removed the Clarity instruments from the separate study
      - Renamed the studies
    
    * Export RLS and QuantAQ without windspeed
    
    * Updated modules for Viking2
    
    * Fully remove windspeed
    
    * Correct filename
    
    ---------
    
    Co-authored-by: Stuart Lacy <sl561@login2.viking2.yor.alces.network>

--- export-for-CEDA/export_lcs_create_joblist.R
+++ export-for-CEDA/export_lcs_create_joblist.R
@@ -13,19 +13,18 @@ con <- dbConnect(RPostgres::Postgres(),
 )
 
 lcs_jobs <- tbl(con, "lcsinstrument") |>
-    filter(company != 'RLS') |>
+    filter(company %in% c('RLS', 'QuantAQ')) |>
     inner_join(tbl(con, "deployment"), by="instrument") |>
     inner_join(tbl(con, "sensor"), by="instrument") |>
-    filter(measurand != 'SO2') |>
+    filter(measurand != 'SO2', measurand != 'WindSpeed') |>
     distinct(instrument, measurand, location) |>
     collect() |>
     mutate(measurand = gsub("PM.+", "PM", measurand),
            measurand = gsub("Temperature", "Met", measurand),
            measurand = gsub("Pressure", "Met", measurand),
-           measurand = gsub("WindSpeed", "Met", measurand),
            measurand = gsub("RelHumidity", "Met", measurand)) |>
     distinct(instrument, measurand, location) |>
-    mutate(out_dir=LCS_DIR, version=1) |>
+    mutate(out_dir=LCS_DIR, version=ifelse(grepl("^Atm", instrument), 1, 2)) |>
     select(instrument, measurand, location, out_dir, version)
 
 write_csv(lcs_jobs, "lcs_job_list.csv")
--- export-for-CEDA/export_lcs_launcher_csv.sh
+++ export-for-CEDA/export_lcs_launcher_csv.sh
@@ -10,12 +10,11 @@
 #SBATCH --mem-per-cpu=4G
 #SBATCH --output=logs/output
 #SBATCH --error=logs/error
-#SBATCH --array=1-278
+#SBATCH --array=5,12,18,26,31,39,53,64,67,79,82,84,85,89,165,178
 
 echo "Loaded script"
 ml load R/4.3.2-gfbf-2023a
 ml load PostgreSQL/16.1-GCCcore-12.3.0
-ml load netCDF/4.9.2-gompi-2023a
 echo "Loaded modules"
-Rscript --vanilla export_lcs_worker.R $SLURM_ARRAY_TASK_ID
+Rscript --vanilla export_lcs_worker_csv.R $SLURM_ARRAY_TASK_ID
 echo "Ran job"
